fc65b518d167b9407bf8637e0a96ace1eac5563d
msantos <msantos@goodreads.com>
2014-01-10 13:50:18 -0800

    Move Rails files up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treasure::Application.config.secret_token = 'c5496c6c4036a49add7b4b3d8792960061faed1afa9bd2817e869194e81fa149f3bcbaeaa37a3be8939aeb81b1740f6306c3bf9c7a6489b296274a8b8e67f3ec'
