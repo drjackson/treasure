34416eda77d300cdb51ef423761c3c8c1cd7d530
sambullardsisken <sambullardsisken@gmail.com>
2014-01-10 16:34:15 -0800

    fill in controller actions

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant
+  # attr_accessible :address, :name, :url
+  attr_reader :address, :name, :url
+
+  def initialize(hash)
+  	@name = hash[:name]
+  	@address = hash[:address]
+  	@url = hash[:url]
+  end
+end
