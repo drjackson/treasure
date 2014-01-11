14552d24cf29684361dfa6a5fd251d94a41332f4
sambullardsisken <sambullardsisken@gmail.com>
2014-01-10 18:18:16 -0800

    remove helpers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,12 @@ class RestaurantsController < ApplicationController
 	def index
 		@restaurants = REDIS.hgetall('restaurants').values.map{|r| Marshal::load(r)}
 	end
-  helper_method :index
 
 	def create
 		restaurant = Restaurant.new(params[:restaurant])
 		index = (REDIS.hlen 'restaurants') + 1
     REDIS.hmset 'restaurants', "restaurant_#{index}", Marshal::dump(restaurant)
 	end
-  helper_method :create
 
 	def show
 		id = params[:id]
@@ -24,5 +22,4 @@ class RestaurantsController < ApplicationController
 			format.json { render :json => Marshal::load(restaurant)}
 		end
 	end
-  helper_method :random
 end
