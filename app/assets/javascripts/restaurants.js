15347420187642ebfe2f1521bf1098dc21ee263f
msantos <msantos@goodreads.com>
2014-01-17 17:03:47 -0800

    Refactor JS and CSS to handle more treasure chests and display info more clearly

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  $(".treasure1").click(function() {
+  function showText() {
+    $(this).addClass("treasure-open");
     $(this).find(".text").show();
-  });
-
-  $(".treasure2").click(function() {
-    $(this).find(".text").show();
-  });
+    $(this).siblings().css("opacity", "0.5");
+  }
+  
+  $(".treasure1").click(showText);
+  $(".treasure2").click(showText);
+  $(".treasure3").click(showText);
 });
-
